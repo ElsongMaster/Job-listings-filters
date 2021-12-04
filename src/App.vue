508c018dde3d48b681dd007504779705658151d8
ElsongMaster <elsongmaster@gmail.com>
2021-12-04 14:06:01 +0100

    fin exo

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,129 @@
 <template>
   <div id="app">
-
+    <div class="bg-img"></div>
+    <div class="search-tag">
+      <div class="tagChoose-container">
+        <div
+          class="tag-choose"
+          v-for="(tag, index) in tagNameToFilter"
+          :key="index"
+        >
+          <span class="tag">{{ tag }}</span
+          ><i class="fas fa-times" @click="deleteTag(tag)"></i>
+        </div>
+      </div>
+      <div class="clear-container" @click="clear()">
+        <span>clear</span>
+      </div>
+    </div>
     <div class="container-job">
-      <Jobcard v-for="item,index in getArrayJson" :key="index" :job="item"/>
+      <Jobcard
+        v-for="(item, index) in getArrayJson"
+        :key="index"
+        :job="item"
+        @getTag="update"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import * as dataJson from "../exo-ressources/data.json"
-import Jobcard from "./components/Jobcard.vue"
+import * as dataJson from "../exo-ressources/data.json";
+import Jobcard from "./components/Jobcard.vue";
 export default {
-  name: 'App',
-  components:{
-    Jobcard
-  },
-  data(){
-    return{
-      dataTab: this.getArrayJson()
-    }
+  name: "App",
+  components: {
+    Jobcard,
   },
 
-  computed:{
-    getArrayJson(){
-      var tab = []
-      for(let key in dataJson){
-        for(let key1 in dataJson[key] ){
-          if(key1 =="logo"){
-            dataJson[key][key1].logo = require(dataJson[key][key1])
+  data() {
+    return {
+      tagNameToFilter: [],
+    };
+  },
+
+  computed: {
+    getArrayJson() {
+      var tab = [];
+      for (let key in dataJson) {
+        for (let key1 in dataJson[key]) {
+          if (key1 == "logo") {
+            dataJson[key][key1].logo = require(dataJson[key][key1]);
           }
-          tab.push(dataJson[key][key1])
+          tab.push(dataJson[key][key1]);
         }
       }
 
       return tab;
-      
+    },
+  },
+  methods: {
+    filterTag(tag) {
+      var tabJobcard = Array.from(
+        document.getElementsByClassName("jobcard-content")
+      );
+      let tabLanguages = null;
+      let tabTools = null;
+      tabJobcard.forEach((elt) => {
+        console.log(elt.dataset.languages);
+        if (elt.dataset.languages.search(",") > 0) {
+          tabLanguages = elt.dataset.languages.split(",");
+        } else {
+          tabLanguages = [elt.dataset.languages];
+        }
+        if (elt.dataset.tools.search(",") > 0) {
+          tabTools = elt.dataset.tools.split(",");
+        } else {
+          tabTools = [elt.dataset.tools];
+        }
+
+        if (
+          elt.dataset.role != tag &&
+          elt.dataset.level != tag &&
+          !tabLanguages.includes(tag) &&
+          !tabTools.includes(tag)
+        ) {
+          elt.classList.add("hide");
+        } else if (elt.classList.contains("hide")) {
+          elt.classList.remove("hide");
+        }
+      });
+    },
+
+    update(tag) {
+      if(! this.tagNameToFilter.includes(tag))
+      this.tagNameToFilter.push(tag);
+      this.filterTag(tag);
+    },
+
+    deleteTag(tag) {
+      this.tagNameToFilter.splice(this.tagNameToFilter.indexOf(tag), 1);
+      this.tagNameToFilter.forEach(tag=>{
+        this.filterTag(tag)
+      })
+
+    },
+
+    clear(){
+      this.tagNameToFilter.splice(0)
+      var tabJobcard = Array.from(document.getElementsByClassName("jobcard-content")
+      );
+      tabJobcard.forEach(elt=>{
+        if (elt.classList.contains("hide")) {
+                  elt.classList.remove("hide");
+                }
+
+      })
     }
-  }
-}
-
-
+  },
+};
 </script>
 
 <style scoped lang="scss">
-
-#app{
+.hide {
+  display: none;
+}
+#app {
   margin: 0;
   padding: 20px;
   width: 100vw;
@@ -52,5 +131,78 @@ export default {
   min-height: 100vh;
   background-color: rgb(225, 248, 250);
 
+  .bg-img {
+    background-image: url("./assets/bg-header-desktop.svg");
+    background-repeat: no-repeat;
+    position: absolute;
+    top: 0;
+
+    width: 100vw;
+    height: 30vh;
+    background-color: rgb(91, 166, 162);
+  }
+  .search-tag {
+    position: absolute;
+    top: 25vh;
+    left: 16%;
+    width: 68vw;
+    height: 60px;
+    background-color: white;
+    z-index: 3;
+    border-radius: 5px;
+    display: flex;
+    padding: 10px;
+  }
+  .tagChoose-container {
+    width: 80%;
+    height: 100%;
+    padding: 5px;
+    display: flex;
+    align-items: center;
+
+    .tag-choose {
+      margin-right: 10px;
+      .tag {
+        display: inline-block;
+        background-color: rgb(225, 248, 250);
+        color: rgb(91, 166, 162);
+        height: 100%;
+        padding: 0.4em;
+        font-size: 13px;
+        font-weight: bold;
+        // border: 2px solid;
+      }
+      i {
+        color: white;
+        padding: 7px;
+        background-color: rgb(91, 166, 162);
+      }
+      i:hover {
+        cursor: url("data:image/x-icon;base64,AAACAAEAICACAAAAAAAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAgAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/gAAAf4AAAPVAAAH1QAAB9WAAA3VgAAd/4AAGbaAAAG2gAABtgAAAYAAAAGAAAABgAAAAYAAAAAAAAA//////////////////////////////////////////////////////////////////////////////////////gD///4A///8AP//+AB///AAf//wAD//4AA//8AAP//AAD//5AA///wAf//8Af///D////w////8P////n///8="),
+          auto;
+        background-color: black;
+        color: rgba(255, 255, 255, 0.822);
+      }
+    }
+  }
+  .clear-container {
+    width: 20%;
+    padding: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    span {
+      color: rgb(91, 166, 162);
+      font-weight: bold;
+      text-decoration: underline;
+    }
+    span:hover {
+      cursor: url("data:image/x-icon;base64,AAACAAEAICACAAAAAAAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAgAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/gAAAf4AAAPVAAAH1QAAB9WAAA3VgAAd/4AAGbaAAAG2gAABtgAAAYAAAAGAAAABgAAAAYAAAAAAAAA//////////////////////////////////////////////////////////////////////////////////////gD///4A///8AP//+AB///AAf//wAD//4AA//8AAP//AAD//5AA///wAf//8Af///D////w////8P////n///8="),
+        auto;
+    }
+  }
+  .container-job {
+    margin-top: 40vh;
+  }
 }
 </style>
--- src/components/Jobcard.vue
+++ src/components/Jobcard.vue
@@ -1,174 +1,179 @@
 <template>
-    <div :class="job.featured?'jobcard-content border-featured':'jobcard-content'">
-        <div class="img-content">
-            
-            <img v-if="job.id == 1" src="../assets/photosnap.svg" alt="">
-            <img v-else-if="job.id == 2" src="../assets/manage.svg" alt="">
-            <img v-else-if="job.id == 3" src="../assets/account.svg" alt="">
-            <img v-else-if="job.id == 4" src="../assets/myhome.svg" alt="">
-            <img v-else-if="job.id == 5" src="../assets/loop-studios.svg" alt="">
-            <img v-else-if="job.id == 6" src="../assets/faceit.svg" alt="">
-            <img v-else-if="job.id == 7" src="../assets/shortly.svg" alt="">
-            <img v-else-if="job.id == 8" src="../assets/insure.svg" alt="">
-            <img v-else-if="job.id == 9" src="../assets/eyecam-co.svg" alt="">
-            <img v-else-if="job.id == 10" src="../assets/the-air-filter-company.svg" alt="">
-        </div>
-        <div class="details-job">
-            <p class="header-details"><span class="company">{{job.company}}</span> <span  v-if="job.new" class="new">New !</span> <span v-if="job.featured" class="featured"> Featured</span></p>
-            <a href="" class="position">{{job.position}}</a>
-            <div class="time-content">
-                <span class="postedAt">{{ job.postedAt}}</span>
-                <ul class="list">
-                    <li>{{job.contract}}</li>
-                    <li>{{job.location}}</li>
-                </ul>
-            </div>
-        </div>
-        <div class="tag-job">
-            <p class="tag-content">
-                <span class="tag">{{job.role}}</span><span class="tag">{{job.level}}</span>
-                <span class="tag" v-for="language,index in job.languages" :key="index">{{language}}</span>
-            </p>
-
-        </div>
+  <div
+    :class="
+      job.featured ? 'jobcard-content border-featured' : 'jobcard-content'
+    "
+    :data-role="job.role"
+    :data-level="job.level"
+    :data-languages="job.languages"
+    :data-tools="job.tools"
+  >
+    <div class="img-content">
+      <img :src="require('@/assets/' + job.logo)" alt="" />
     </div>
+    <div class="details-job">
+      <p class="header-details">
+        <span class="company">{{ job.company }}</span>
+        <span v-if="job.new" class="new">New !</span>
+        <span v-if="job.featured" class="featured"> Featured</span>
+      </p>
+      <a href="" class="position">{{ job.position }}</a>
+      <div class="time-content">
+        <span class="postedAt">{{ job.postedAt }}</span>
+        <ul class="list">
+          <li>{{ job.contract }}</li>
+          <li>{{ job.location }}</li>
+        </ul>
+      </div>
+    </div>
+    <div class="tag-job">
+      <p class="tag-content">
+        <span class="tag" @click="sendTagChoose(job.role)">{{ job.role }}</span
+        ><span class="tag" @click="sendTagChoose(job.level)">{{ job.level }}</span>
+        <span
+          class="tag"
+          v-for="(language, index) in job.languages"
+          :key="index"
+           @click="sendTagChoose(language)">{{ language }}</span
+        >
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"Jobcard",
-    props:{
-        job:Object
-    },
+  name: "Jobcard",
+  props: {
+    job: Object,
+  },
 
-    // computed:{
-    //     getImgUrl(){
-    //         let url = rthis.job.logo);
-    //         return url;
-    //     }
-    // }
-}
+  methods: {
+    sendTagChoose(tag) {
+    //   console.log(tag);
+      this.$emit('getTag', tag);
+      // this.$emit('update-cart', item)
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-    .jobcard-content{
-        height: 125px;
-        width: 70%;
-        background-color: white;
-        margin: 30px auto;
-        border-radius: 10px;
-        display: flex;
+.jobcard-content {
+  height: 125px;
+  width: 70%;
+  background-color: white;
+  margin: 30px auto;
+  border-radius: 10px;
+  display: flex;
+  padding: 5px;
+  box-shadow: 5px 5px 5px 5px rgba(0, 0, 0, 0.2);
+  transition: 0.2s ease-in-out;
+
+  .img-content {
+    width: 15%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 10px;
+  }
+  .details-job {
+    padding: 5px;
+    // border:2px solid;
+    width: 40%;
+
+    .header-details {
+      margin: 10px 0;
+
+      .company,
+      .new,
+      .featured {
+        font-size: 13px;
+        font-weight: bold;
+        color: white;
+      }
+      .company {
+        color: rgb(94, 168, 167);
+      }
+      .new,
+      .featured {
+        text-transform: uppercase;
         padding: 5px;
-        box-shadow: 5px 5px 5px 5px rgba(0,0,0,0.2);
-
-
-        .img-content{
-            width: 15%;
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            padding: 10px;
-
-        }
-        .details-job{
-            padding: 5px;
-            // border:2px solid;
-            width: 40%;
-
-            .header-details{
-
-                margin: 10px 0;
-                
-    
-                .company,.new,.featured{
-                    font-size: 13px;
-                    font-weight: bold;
-                    color: white;
-                }
-                .company{
-                    
-                    color: rgb(94,168,167);
-                }
-                .new,.featured{
-                    text-transform: uppercase;
-                    padding: 5px;
-                    border-radius: 40%;
-                    
-                }
-                .new{
-      
-                    background-color:rgb(94,168,167);
-                }
-                .featured{
-                   background-color:black
-                }
-            }
-            
-        }
-        .position{
-            color: rgb(41,50,49);
-            font-weight: bold;
-            
-        }
-        .position:hover{
-           cursor: url('data:image/x-icon;base64,AAACAAEAICACAAAAAAAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAgAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/gAAAf4AAAPVAAAH1QAAB9WAAA3VgAAd/4AAGbaAAAG2gAABtgAAAYAAAAGAAAABgAAAAYAAAAAAAAA//////////////////////////////////////////////////////////////////////////////////////gD///4A///8AP//+AB///AAf//wAD//4AA//8AAP//AAD//5AA///wAf//8Af///D////w////8P////n///8='), auto; 
-           color: rgb(94,168,167);
-        }
-        .time-content{
-            display:flex;
-            justify-content: space-between;
-            margin-top: 5px;
-            // border: 2px solid;
-            padding-left: 0;
-            width: 75%;
-
-            *{
-                color: rgb(151, 157, 157);
-                font-size: 13px;
-            }
-            .postedAt{
-                text-align: left;
-                // border: 2px solid;
-                width: 20%;
-
-            }
-            .list{
-                display: flex;
-                justify-content: space-between;
-                width: 65%;
-                list-style-type: disc;
-            }
-
-        }
-        .tag-job{
-            width: 50%;
-            display: flex;
-            align-items: center;
-
-            .tag-content{
-                width: 100%;
-                display: flex;
-                justify-content: space-between;
-
-                .tag{
-                    text-align: center;
-                    padding:5px;
-                    background-color: rgb(225, 248, 250);
-                    color: rgb(94,168,167);
-                    font-weight: bold;
-                }
-                .tag:hover{
-                    cursor: url('data:image/x-icon;base64,AAACAAEAICACAAAAAAAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAgAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/gAAAf4AAAPVAAAH1QAAB9WAAA3VgAAd/4AAGbaAAAG2gAABtgAAAYAAAAGAAAABgAAAAYAAAAAAAAA//////////////////////////////////////////////////////////////////////////////////////gD///4A///8AP//+AB///AAf//wAD//4AA//8AAP//AAD//5AA///wAf//8Af///D////w////8P////n///8='), auto; 
-            
-                    background-color:rgb(94,168,167) ;
-
-                    color: white;
-                }
-            }
-        }
-
+        border-radius: 40%;
+      }
+      .new {
+        background-color: rgb(94, 168, 167);
+      }
+      .featured {
+        background-color: black;
+      }
     }
-    .border-featured{
-        border-left: 5px solid rgb(94,168,167)  !important; 
+  }
+  .position {
+    color: rgb(41, 50, 49);
+    font-weight: bold;
+  }
+  .position:hover {
+    cursor: url("data:image/x-icon;base64,AAACAAEAICACAAAAAAAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAgAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/gAAAf4AAAPVAAAH1QAAB9WAAA3VgAAd/4AAGbaAAAG2gAABtgAAAYAAAAGAAAABgAAAAYAAAAAAAAA//////////////////////////////////////////////////////////////////////////////////////gD///4A///8AP//+AB///AAf//wAD//4AA//8AAP//AAD//5AA///wAf//8Af///D////w////8P////n///8="),
+      auto;
+    color: rgb(94, 168, 167);
+  }
+  .time-content {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 5px;
+    // border: 2px solid;
+    padding-left: 0;
+    width: 75%;
+
+    * {
+      color: rgb(151, 157, 157);
+      font-size: 13px;
     }
+    .postedAt {
+      text-align: left;
+      // border: 2px solid;
+      width: 20%;
+    }
+    .list {
+      display: flex;
+      justify-content: space-between;
+      width: 65%;
+      list-style-type: disc;
+    }
+  }
+  .tag-job {
+    width: 50%;
+    display: flex;
+    align-items: center;
+
+    .tag-content {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+
+      .tag {
+        text-align: center;
+        padding: 5px;
+        background-color: rgb(225, 248, 250);
+        color: rgb(94, 168, 167);
+        font-weight: bold;
+      }
+      .tag:hover {
+        cursor: url("data:image/x-icon;base64,AAACAAEAICACAAAAAAAwAQAAFgAAACgAAAAgAAAAQAAAAAEAAQAAAAAAgAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/gAAAf4AAAPVAAAH1QAAB9WAAA3VgAAd/4AAGbaAAAG2gAABtgAAAYAAAAGAAAABgAAAAYAAAAAAAAA//////////////////////////////////////////////////////////////////////////////////////gD///4A///8AP//+AB///AAf//wAD//4AA//8AAP//AAD//5AA///wAf//8Af///D////w////8P////n///8="),
+          auto;
+
+        background-color: rgb(94, 168, 167);
+
+        color: white;
+      }
+    }
+  }
+}
+.jobcard-content:hover {
+  transform: translateY(10px);
+}
+.border-featured {
+  border-left: 5px solid rgb(94, 168, 167) !important;
+}
 </style>
